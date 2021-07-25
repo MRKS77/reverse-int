e09edce67b7d6c9a0ef8548daaceb8cad6c872f7
MRKS77 <artxmarkos@gmail.com>
2021-07-25 16:46:52 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  return Number(String(Math.abs(n)).split('').reverse().join(''))
+//   *ALTERNATIVE SOLUTION*
+//
+//   n = Math.abs(n);
+//   let x = 0;
+//   while(n) {
+//       x = (x * 10) + (n % 10);
+//       n = Math.floor(n / 10)
+//   }
+//   return x
 }
